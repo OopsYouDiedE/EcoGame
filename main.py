0ec53c59da22d20ad66d662e473eaf1528dbb36c
zhouzinimg <69280251+zhouzinimg@users.noreply.github.com>
2024-03-06 21:05:43 +0800

    nothing

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ import aioshutil
 from aiocsv import AsyncReader, AsyncDictReader, AsyncWriter, AsyncDictWriter
 
 class CoreEconomySystem(interactions.Extension):
+    import database_manager
     module_base: interactions.SlashCommand = interactions.SlashCommand(
         name="core_economy_system",
         description="Minimize Core For Economy Simulation"
